298c59ebecdc9909df7ecc918165a161516e8860
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-12-23 15:16:12 +0530

    indexed db checked for sesseions

---
+++ scripts/checksession.js
@@ -0,0 +1,13 @@
+
+
+var checkSession = function(){
+    var storeApp = getStore('application','readonly');
+    var result =  storeApp.get(1);
+   
+   result.onsuccess = function(e){
+       console.log(e);
+       if(!e.target.result) helpers.show(splashDiv);
+       else helpers.hide(splashDiv);
+   };
+    
+};
