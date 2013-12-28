da708923d756dcfca97a0cc75ece7935cc1bd35d
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-12-28 20:05:44 +0530

    Image preview working

--- scripts/profile.index.js
+++ scripts/profile.index.js
@@ -33,6 +33,8 @@
 
 }).call(this);
 
+ var preview = document.getElementById("imagepreview");
+var userName = document.getElementById("name");
 function autoGrow (oField) {
   if (oField.scrollHeight > oField.clientHeight) {
     oField.style.height = oField.scrollHeight + "px";
@@ -40,9 +42,16 @@ function autoGrow (oField) {
 };
 
 function createImage(text){
+   preview.innerHTML = "";
     text.autoLink({
 	callback: function(url) {
-	    return /\.(gif|png|jpe?g)$/i.test(url) ? '<img src="' + url + '">' : null;
+	   return /\.(gif|png|jpe?g)$/i.test(url) ?addImage(url) : null;
 	}
     });
 };
+
+function addImage(url){
+preview.display = 'block';
+preview.innerHTML = '<img id ="profileimage" src="' + url + '">';
+userName.focus();
+}
--- scripts/profile.js
+++ scripts/profile.js
@@ -1,3 +1,5 @@
+ var preview = document.getElementById("imagepreview");
+var userName = document.getElementById("name");
 function autoGrow (oField) {
   if (oField.scrollHeight > oField.clientHeight) {
     oField.style.height = oField.scrollHeight + "px";
@@ -5,10 +7,16 @@ function autoGrow (oField) {
 };
 
 function createImage(text){
+   preview.innerHTML = "";
     text.autoLink({
 	callback: function(url) {
-	    return /\.(gif|png|jpe?g)$/i.test(url) ? '<img src="' + url + '">' : null;
+	   return /\.(gif|png|jpe?g)$/i.test(url) ?addImage(url) : null;
 	}
     });
 };
 
+function addImage(url){
+preview.display = 'block';
+preview.innerHTML = '<img id ="profileimage" src="' + url + '">';
+userName.focus();
+}
