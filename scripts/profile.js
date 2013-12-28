2d54f3cf4140f18f43878f1b6a067f79a160f4b3
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-12-28 20:32:01 +0530

    Plumbing to save the details into the indexed db

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,22 +1,35 @@
- var preview = document.getElementById("imagepreview");
+var preview = document.getElementById("imagepreview");
 var userName = document.getElementById("name");
+var openRequest = indexedDB.open("wrinq", 1);
+var database;
+
 function autoGrow (oField) {
-  if (oField.scrollHeight > oField.clientHeight) {
-    oField.style.height = oField.scrollHeight + "px";
-  }
+    if (oField.scrollHeight > oField.clientHeight) {
+	oField.style.height = oField.scrollHeight + "px";
+    }
 };
 
 function createImage(text){
-   preview.innerHTML = "";
+    preview.innerHTML = "";
     text.autoLink({
 	callback: function(url) {
-	   return /\.(gif|png|jpe?g)$/i.test(url) ?addImage(url) : null;
+	    return /\.(gif|png|jpe?g)$/i.test(url) ?addImage(url) : null;
 	}
     });
 };
 
 function addImage(url){
-preview.display = 'block';
-preview.innerHTML = '<img id ="profileimage" src="' + url + '">';
-userName.focus();
+    preview.display = 'block';
+    preview.innerHTML = '<img id ="profileimage" src="' + url + '">';
+    userName.focus();
 }
+
+openRequest.onupgradeneeded = function(e){
+    database = e.target.result;    
+   
+};
+function getStore(objectStore,permission){
+    var tran = database.transaction([objectStore],permission);
+    var store = tran.objectStore(objectStore);    
+    return store;
+};
