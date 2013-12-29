45b48aeba7705a8ed45f9575a0ae6d4368ef1ecf
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-12-29 10:48:12 +0530

    The profile persisted to idb

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,5 +1,6 @@
 var preview = document.getElementById("imagepreview");
 var userName = document.getElementById("name");
+var about = document.getElementById("about");
 var openRequest = indexedDB.open("wrinq", 1);
 var database;
 
@@ -22,13 +23,35 @@ function addImage(url){
     preview.innerHTML = '<img id ="profileimage" src="' + url + '">';
     userName.focus();
 }
-
-openRequest.onupgradeneeded = function(e){
-    database = e.target.result;    
-   
+openRequest.onsuccess = function(e){
+    database = e.target.result;
+    var  profileStore = getStore('profile','readonly');
+    var result = profileStore.get('master');
+    profileStore.onsuccess = function(e){
+	var profile = e.target.result;
+	if(!profile) return;
+	preview.value = profile.pic;
+	userName.value = profile.name;
+	about.value = profile.about;
+    };
 };
+
+
+
 function getStore(objectStore,permission){
     var tran = database.transaction([objectStore],permission);
     var store = tran.objectStore(objectStore);    
     return store;
+};
+
+function updateProfile(){
+var profileStore = getStore('profile','readwrite');
+    var image = document.getElementById("profileimage");
+    var item = {
+	"pic" :image? image.src:"",
+	"about" : about.value,
+	"name" : userName.value,
+	"upgraded":true
+    };
+    profileStore.add("item","master");
 };
