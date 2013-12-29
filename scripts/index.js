a7809822cc4d1e946bbf00d0bccc5c1f72bab864
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2013-12-29 19:40:21 +0530

    added a dynamic reply box

--- scripts/index.js
+++ scripts/index.js
@@ -199,8 +199,9 @@ var domElements = {
 
     'signUpForm' :'<form  id ="signUpForm" onsubmit="submitAjax(event,this)"><p><input type="text" name="username" value="" placeholder= "username" onfocusout="checkUser(this)" onfocus = "clearMessages()" required/></p><p><input type="password" name="password" value="" placeholder="password"  required/></p><p><input type="submit" id="submitButton"  value="sign up" disabled/></p></form><p id ="message"></p><p><span class ="underline-spans" onclick = "loginClick()">or login<span></p>',
 
-    'loginForm': '<form  method="POST" id="loginForm" onsubmit="submitAjax(event,this)"><p><input type="text" name="username" value="" placeholder="username"  required/></p><p><input type="password" name="password" value="" placeholder="password"  required/></p><p><input type="submit" id="submitButton" name="" value="login"/></p></form><p id= "message"></p><p ><span class ="underline-spans" onclick ="signUpClick()">or sign-up<span></p>'
+    'loginForm': '<form  method="POST" id="loginForm" onsubmit="submitAjax(event,this)"><p><input type="text" name="username" value="" placeholder="username"  required/></p><p><input type="password" name="password" value="" placeholder="password"  required/></p><p><input type="submit" id="submitButton" name="" value="login"/></p></form><p id= "message"></p><p ><span class ="underline-spans" onclick ="signUpClick()">or sign-up<span></p>',
 
+'commentBox':'<textarea rows="10"  style ="overflow: hidden; width:100%" id="commentbox" placeholder="reply" onkeyup = "autoGrow(this)"></textarea><span><button type="button">post</button></span><span><button type="button">cancel</button></span>'
 };
 
 
@@ -256,4 +257,15 @@ var clearMessages = function(){
     var message = helpers.id("message");
     message.innerHTML = '';
    
+};
+
+function autoGrow (oField) {
+    if (oField.scrollHeight > oField.clientHeight) {
+	oField.style.height = oField.scrollHeight + "px";
+    }
+};
+
+function addCommentBox(element){
+element.style.display = 'none';
+document.getElementsByClassName("comment")[0].innerHTML = domElements.commentBox;
 };
--- scripts/main.js
+++ scripts/main.js
@@ -48,3 +48,14 @@ var clearMessages = function(){
     message.innerHTML = '';
    
 };
+
+function autoGrow (oField) {
+    if (oField.scrollHeight > oField.clientHeight) {
+	oField.style.height = oField.scrollHeight + "px";
+    }
+};
+
+function addCommentBox(element){
+element.style.display = 'none';
+document.getElementsByClassName("comment")[0].innerHTML = domElements.commentBox;
+};
