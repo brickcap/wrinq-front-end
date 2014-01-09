59ff84b5b0658c03b84aa1426fa48471ade1cd7c
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2014-01-10 00:25:40 +0530

    Saved an incoming tag

--- scripts/html.js
+++ scripts/html.js
@@ -27,6 +27,7 @@ var domElements = {
 	};
 	var msg = helpers.output(m.m.m);
 	var tag = m.m.t?m.m.t:'';
+	if(tag) save(tag,"tags");
 	var ms = '<div class="messageBody" data-to="'+m.f+'" data-tag="'+tag+'"><hr style="border-color:#fff"/><p><span>'+det()+'</span><span> <em>'+mDate+mTime+'</em>]</span></p><span>'+msg+'</span><p><span class="details" onclick="showTag(this)">'+tag +'</span>'+to+'</p> <p><button onclick = "addCommentBox(this)">reply</button></p> </div></div>';
 	return ms;
     }
