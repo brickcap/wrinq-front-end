71ae4f9e82e7ee9728ce3c79830368fcf68148c6
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2014-01-09 14:30:23 +0530

    A skeleton for getting tagged messages ready

--- scripts/tag.js
+++ scripts/tag.js
@@ -4,9 +4,28 @@ helpers.hide(sendMessage);
 helpers.hide(messages);
 helpers.hide(conversation);
 helpers.show(tagDiv);
-console.log(tag);
 //buildTag(tag);
 }
 
 function buildTag(t){
+    var mStore = getStore('messages','readonly');
+    var mIndex = mStore.index("tag");
+    var keyRange = IDBKeyRange.only(t);
+    var cursor = mIndex.openCursor(keyRange,'prev');
+    var count = 0;
+    var mStr='';
+    //use cursor.advance(int);
+    cursor.onsuccess = function(e){
+	var item = e.target.result;
+	if(item && count!=10){
+	    item.continue();
+	    item.value.m.p = p;
+	    mStr = mStr + domElements.incomingMessage(item.value);
+	    count++;
+	}
+	if(!item||count===10){
+	    var heading = '<h1 class="center-div">Messages Tagged as '+t+'</h1>';
+	    conversation.innerHTML = heading+mStr;   
+	}
+    };
 }
