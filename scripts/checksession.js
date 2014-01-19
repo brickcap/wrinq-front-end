b6154592e2767125882f3722ab4f7987ba632f50
Akshat Jiwan Sharma <akshat_fullmetal@yahoo.co.in>
2014-01-19 18:50:43 +0530

    Username printed at screen

--- scripts/checksession.js
+++ scripts/checksession.js
@@ -11,11 +11,13 @@ var checkSession = function(){
 	helpers.hide(formDiv);
 	var profileStore = getStore("profile",'readonly');
 	var profile = profileStore.get("master"); 
+	var user = localStorage.getItem("user");
 	profile.onsuccess = function(e){
 	    if(!e.target.result){
-		messageDiv.innerHTML = '<a href="/editprofile.html">create a profile</a>';
+		messageDiv.innerHTML = '<a href="/editprofile.html">create a profile '+user+'</a>';
 		return;
 	    }
+	    messageDiv.innerHTML = '<a href="/editprofile.html">'+user+'</a>';
 	    prf = e.target.result;
 	};
 	helpers.buildMessages();
